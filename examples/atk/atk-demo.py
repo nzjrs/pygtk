444d24ae7598cf61dce659dba89fa7ea8c9b802c
Matt Wilson <msw@src.gnome.org>
2001-11-28 23:56:31 +0000

    minor tweaks to actually use the api right

--- examples/atk/atk-demo.py
+++ examples/atk/atk-demo.py
@@ -8,14 +8,16 @@ win.connect('destroy', lambda win: gtk.main_quit())
 
 button = gtk.Button('Quit')
 button.connect('pressed', lambda button: gtk.main_quit())
-buttonAccess = button.get_accessible()
-buttonAccess.set_description('Be careful, clicking this button will exit')
+atk_button = button.get_accessible()
+atk_button.set_description('Be careful, clicking this button will exit')
 
 label = gtk.Label('This label describes a button')
-labelAccess = label.get_accessible()
-labelAccess.set_description('This is a useless label')
+atk_label = label.get_accessible()
+atk_label.set_description('This is a useless label')
+relation_set = atk_label.ref_relation_set()
 
-relation = atk.Relation((buttonAccess, labelAccess), atk.RELATION_LABEL_FOR)
+relation = atk.Relation((atk_button,), atk.RELATION_LABEL_FOR)
+relation_set.add(relation)
 
 box = gtk.HBox()
 box.pack_start(label)
