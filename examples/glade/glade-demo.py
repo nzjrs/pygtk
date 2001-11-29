31a5a3b659d225cdd64c2f565133fd9b0d89374f
Matt Wilson <msw@src.gnome.org>
2001-11-29 22:56:08 +0000

    rework glade demo and demo files to be glade2

---
+++ examples/glade/glade-demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+import gtk
+from gtk import glade
+
+if len(sys.argv) > 1:
+    fname = sys.argv[1]
+else:
+    fname = 'test.glade'
+
+# create widget tree ...
+xml = glade.XML(fname, None, None)
+
+def gtk_main_quit(*args):
+    gtk.main_quit()
+
+xml.signal_autoconnect(locals())
+
+gtk.main()
