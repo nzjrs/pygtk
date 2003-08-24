0390afd4cffd1d70133a983e3373e1c0f4bd1955
James Henstridge <james@daa.com.au>
2003-08-24 11:39:57 +0000

    add news items.
    2003-08-24  James Henstridge  <james@daa.com.au>
    
        * NEWS: add news items.
    
        * configure.in: update version number.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from dsextras import getoutput, have_pkgconfig, list_files, \
 
 MAJOR_VERSION = 1
 MINOR_VERSION = 99
-MICRO_VERSION = 17
+MICRO_VERSION = 18
 
 VERSION = "%d.%d.%d" % (MAJOR_VERSION,
                         MINOR_VERSION,
