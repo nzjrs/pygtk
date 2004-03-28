1126c887ddf916f9d2ac54a4518cea6251786cbc
Johan Dahlin <zilch@src.gnome.org>
2004-03-28 16:20:11 +0000

    Forgot to commit this yesterday

--- gtk/pygtkcellrenderer.h
+++ gtk/pygtkcellrenderer.h
@@ -19,14 +19,15 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA
  */
-#include <gtk/gtk.h>
 
-#define PYGTK_TYPE_GENERIC_CELL_RENDERER (pygtk_generic_cell_renderer_get_type())
-#define PYGTK_GENERIC_CELL_RENDERER(object) (G_TYPE_CHECK_INSTANCE_CAST((object), PYGTK_TYPE_GENERIC_CELL_RENDERER, PyGtkGenericCellRenderer))
-#define PYGTK_GENERIC_CELL_RENDERER_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), PYGTK_TYPE_GENERIC_CELL_RENDERER, PyGtkGenericCellRendererClass))
-#define PYGTK_IS_GENERIC_CELL_RENDERER(object) (G_TYPE_CHECK_INSTANCE_TYPE((object), PYGTK_TYPE_GENERIC_CELL_RENDERER))
+#include <gtk/gtkcellrenderer.h>
+
+#define PYGTK_TYPE_GENERIC_CELL_RENDERER            (pygtk_generic_cell_renderer_get_type())
+#define PYGTK_GENERIC_CELL_RENDERER(object)         (G_TYPE_CHECK_INSTANCE_CAST((object), PYGTK_TYPE_GENERIC_CELL_RENDERER, PyGtkGenericCellRenderer))
+#define PYGTK_GENERIC_CELL_RENDERER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), PYGTK_TYPE_GENERIC_CELL_RENDERER, PyGtkGenericCellRendererClass))
+#define PYGTK_IS_GENERIC_CELL_RENDERER(object)      (G_TYPE_CHECK_INSTANCE_TYPE((object), PYGTK_TYPE_GENERIC_CELL_RENDERER))
 #define PYGTK_IS_GENERIC_CELL_RENDERER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), PYGTK_TYPE_GENERIC_CELL_RENDERER))
-#define PYGTK_GENERIC_CELL_RENDERER_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), PYGTK_TYPE_GENERIC_CELL_RENDERER, PyGtkGenericCellRendererClass))
+#define PYGTK_GENERIC_CELL_RENDERER_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), PYGTK_TYPE_GENERIC_CELL_RENDERER, PyGtkGenericCellRendererClass))
 
 typedef struct _PyGtkGenericCellRenderer PyGtkGenericCellRenderer;
 typedef struct _PyGtkGenericCellRendererClass PyGtkGenericCellRendererClass;
@@ -37,7 +38,6 @@ struct _PyGtkGenericCellRenderer {
 
 struct _PyGtkGenericCellRendererClass {
     GtkCellRendererClass parent_class;
-
 };
 
 GType            pygtk_generic_cell_renderer_get_type (void);
--- gtk/pygtktreemodel.h
+++ gtk/pygtktreemodel.h
@@ -19,14 +19,15 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA
  */
-#include <gtk/gtk.h>
 
-#define PYGTK_TYPE_GENERIC_TREE_MODEL (pygtk_generic_tree_model_get_type())
-#define PYGTK_GENERIC_TREE_MODEL(object) (G_TYPE_CHECK_INSTANCE_CAST((object), PYGTK_TYPE_GENERIC_TREE_MODEL, PyGtkGenericTreeModel))
-#define PYGTK_GENERIC_TREE_MODEL_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), PYGTK_TYPE_GENERIC_TREE_MODEL, PyGtkGenericTreeModelClass))
-#define PYGTK_IS_GENERIC_TREE_MODEL(object) (G_TYPE_CHECK_INSTANCE_TYPE((object), PYGTK_TYPE_GENERIC_TREE_MODEL))
+#include <gtk/gtktreemodel.h>
+
+#define PYGTK_TYPE_GENERIC_TREE_MODEL            (pygtk_generic_tree_model_get_type())
+#define PYGTK_GENERIC_TREE_MODEL(object)         (G_TYPE_CHECK_INSTANCE_CAST((object), PYGTK_TYPE_GENERIC_TREE_MODEL, PyGtkGenericTreeModel))
+#define PYGTK_GENERIC_TREE_MODEL_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), PYGTK_TYPE_GENERIC_TREE_MODEL, PyGtkGenericTreeModelClass))
+#define PYGTK_IS_GENERIC_TREE_MODEL(object)      (G_TYPE_CHECK_INSTANCE_TYPE((object), PYGTK_TYPE_GENERIC_TREE_MODEL))
 #define PYGTK_IS_GENERIC_TREE_MODEL_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), PYGTK_TYPE_GENERIC_TREE_MODEL))
-#define PYGTK_GENERIC_TREE_MODEL_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), PYGTK_TYPE_GENERIC_TREE_MODEL, PyGtkGenericTreeModelClass))
+#define PYGTK_GENERIC_TREE_MODEL_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), PYGTK_TYPE_GENERIC_TREE_MODEL, PyGtkGenericTreeModelClass))
 
 typedef struct _PyGtkGenericTreeModel PyGtkGenericTreeModel;
 typedef struct _PyGtkGenericTreeModelClass PyGtkGenericTreeModelClass;
@@ -39,9 +40,7 @@ struct _PyGtkGenericTreeModel {
 
 struct _PyGtkGenericTreeModelClass {
     GObjectClass parent_class;
-
 };
 
-GType pygtk_generic_tree_model_get_type(void);
-
-PyGtkGenericTreeModel *pygtk_generic_tree_model_new(void);
+GType                   pygtk_generic_tree_model_get_type (void);
+PyGtkGenericTreeModel * pygtk_generic_tree_model_new      (void);
