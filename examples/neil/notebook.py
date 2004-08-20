69504e77e5e4fd7c1348085700565ac0c6a8f7d4
John Finlay <finlay@src.gnome.org>
2004-08-20 05:54:41 +0000

    Update
    	* Update

--- examples/neil/notebook.py
+++ examples/neil/notebook.py
@@ -1,63 +1,64 @@
 #!/usr/bin/env python
 
-from gtk import *
+import pygtk
+pygtk.require('2.0')
+import gtk
 
 def destroy(args):
-	window.destroy()
-	mainquit()
+    window.destroy()
+    gtk.main_quit()
 
-
-window = GtkWindow()
+window = gtk.Window()
 window.connect("destroy", destroy)
 window.set_title('Notebook')
 window.set_border_width(0)
-window.set_usize(400, 400)
+window.set_size_request(400, 400)
 
-box1 = GtkVBox()
+box1 = gtk.VBox()
 window.add(box1)
 box1.show()
 
-box2 = GtkVBox(spacing=10)
+box2 = gtk.VBox(spacing=10)
 box2.set_border_width(10)
 box1.pack_start(box2)
 box2.show()
 
-notebook = GtkNotebook()
-notebook.set_tab_pos(POS_TOP)
+notebook = gtk.Notebook()
+notebook.set_tab_pos(gtk.POS_TOP)
 box2.pack_start(notebook)
 notebook.show()
 
 names = ['Background', 'Colors', 'System', 'Setup', 'Samba']
 
 for i in range(len(names)):
-	buffer = names[i]
-	frame = GtkFrame(buffer)
-	frame.set_border_width(10)
-	frame.set_usize(200, 300)
-	frame.set_shadow_type(SHADOW_ETCHED_OUT)
-	frame.show()
-	
-	label = GtkLabel(buffer)
-	frame.add(label)
-	label.show()
+    buffer = names[i]
+    frame = gtk.Frame(buffer)
+    frame.set_border_width(10)
+    frame.set_size_request(200, 300)
+    frame.set_shadow_type(gtk.SHADOW_ETCHED_OUT)
+    frame.show()
 
-	label = GtkLabel(buffer)
-	label.set_padding(2, 2)
-	notebook.append_page(frame, label)
+    label = gtk.Label(buffer)
+    frame.add(label)
+    label.show()
 
-separator = GtkHSeparator()
+    label = gtk.Label(buffer)
+    label.set_padding(2, 2)
+    notebook.append_page(frame, label)
+
+separator = gtk.HSeparator()
 box1.pack_start(separator)
 separator.show()
 
-box3 = GtkVBox(spacing=10)
+box3 = gtk.VBox(spacing=10)
 box3.set_border_width(10)
 box1.pack_start(box3)
 box3.show()
 
-button = GtkButton(label='close')
+button = gtk.Button(label='close')
 box3.pack_start(button)
 button.connect('clicked', destroy)
 button.show()
 
 window.show()
-mainloop()
+gtk.main()
