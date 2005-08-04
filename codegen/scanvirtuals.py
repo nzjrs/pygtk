84c6f128bb6ee2ef0a1dc69d2f514b2c6c9c8e07
Johan Dahlin <johan@src.gnome.org>
2005-08-04 15:44:42 +0000

    Improve parsing, catches a little bit more

--- codegen/scanvirtuals.py
+++ codegen/scanvirtuals.py
@@ -4,19 +4,25 @@ import sys
 
 
 def main():
-    rx = re.compile(r'^\s*(\w+)\s*\(\s*\*\s*(\w+)\s*\)\s*\(([^()]*)\);',
+    rx = re.compile(r'^\s*([\w\s\*]+)\(\s*\*\s*(\w+)\s*\)\s*\(([^()]*)\);',
                     re.DOTALL|re.MULTILINE)
     for f in sys.argv[1:]:
         #print ";; From", f
         buf = file(f).read()
         for m in rx.findall(buf):
-            return_type =  m[0]
+            return_type =  m[0].strip()
+            if 'typedef' in return_type:
+                continue
             if return_type == 'void':
                 return_type = 'none'
+            return_type = return_type.replace(' ', '')
             virtual_name =  m[1]
             if 'reserved' in virtual_name:
                 continue
             params = []
+            if not m[2]:
+                print >> sys.stderr, repr(m)
+                continue
             for param in map(str.strip, m[2].split(',')):
                 if '*' in param:
                     tokens = param.split('*')
@@ -31,6 +37,8 @@ def main():
                     pname = pname.replace('[]', '')
                     ptype += '[]'
                 params.append((ptype, pname))
+            if not params:
+                continue
             objname = params[0][0].replace('*', '')
             print '(define-virtual', virtual_name
             print '  (of-object "%s")' % objname
