53df8ba50db2bb435762037c9c8544178696b274
John Ehresman <jpe@wingware.com>
2006-01-20 21:27:27 +0000

    Bump ref count on x11 so destroy() works as expected
    2006-01-20  John Ehresman <jpe@wingware.com>
    
    	* gtk/gdk.override (_wrap_gdk_window_new): Bump ref count
    	on x11 so destroy() works as expected
    
    	* tests/test_gdk.py: test for the above

--- tests/test_gdk.py
+++ tests/test_gdk.py
@@ -1,6 +1,13 @@
 import unittest
+import gc
 
 from common import gtk
+
+class CallOnDel:
+    def __init__(self, callback):
+        self.callback = callback
+    def __del__(self):
+        self.callback()
 
 class GdkTest(unittest.TestCase):
     def testBitmapCreateFromData(self):
@@ -10,3 +17,20 @@ class GdkTest(unittest.TestCase):
         black = gtk.gdk.color_parse('black')
         gtk.gdk.pixmap_create_from_data(None, '\x00', 1, 1, 1,
                                         black, black)
+
+    def testWindow(self):
+        common = {'finalized': False}
+        def on_finalize():
+            common['finalized'] = True
+        w = gtk.gdk.Window(None, 200, 200, gtk.gdk.WINDOW_TEMP, 0, 0)
+        w.set_data('foo', CallOnDel(on_finalize))
+        w.destroy()
+        while gtk.events_pending():
+            gtk.main_iteration(block=False)
+        del w
+
+        # Note that this depends on the mainloop processing an X event so
+        # if might fail if the timing is off
+        while gc.collect():
+            pass
+        assert common['finalized']
