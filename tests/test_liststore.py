8f80074653f747a42baba5c062fe731712ebceec
Johan Dahlin <johan@src.gnome.org>
2006-04-05 22:00:33 +0000

    Allow callback to be set to None, found by Patrick O'Brien
        * gtk/gtktreeview.override
        (_wrap_gtk_tree_sortable_set_default_sort_func): Allow callback
        to be set to None, found by Patrick O'Brien
    
        * tests/test_liststore.py (ListStoreTest.testSetDefaultSortFunc):
        Add test

--- tests/test_liststore.py
+++ tests/test_liststore.py
@@ -20,5 +20,17 @@ class ListStoreTest(unittest.TestCase):
         self.assertEqual(store[0][0], 1)
         self.assertEqual(store[1][0], 2)
 
+    # TreeSortable, but...
+
+    def testSetDefaultSortFunc(self):
+        store = gtk.ListStore(int)
+        self.failIf(store.has_default_sort_func())
+        store.set_default_sort_func(lambda x: None)
+        store.set_sort_column_id(0, gtk.SORT_ASCENDING)
+
+        self.failUnless(store.has_default_sort_func())
+        store.set_default_sort_func(None)
+        self.failIf(store.has_default_sort_func())
+
 if __name__ == '__main__':
     unittest.main()
