0e36ff285575da090d473232c285c547579a4be4
Gustavo J. A. M. Carneiro <gjc@src.gnome.org>
2006-06-14 18:20:39 +0000

    Bug 344845 – Missing gdk-base-types.defs causes gedit build to fail

--- codegen/defsparser.py
+++ codegen/defsparser.py
@@ -20,7 +20,7 @@ class IncludeParser(scmexpr.Parser):
         else:
             inc_path = [os.path.dirname(self.filename)] + include_path
             for filename in [os.path.join(path_entry, input_filename)
-                             for path_entry in include_path]:
+                             for path_entry in inc_path]:
                 if not os.path.exists(filename):
                     continue
                 # set self.filename to the include name, to handle recursive includes
