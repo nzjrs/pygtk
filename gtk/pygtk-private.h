30bd7b96f5c36f7b9b34b4c457c779a8b99b3494
Gian Mario Tagliaretti <gianmt@src.gnome.org>
2008-05-18 19:07:37 +0000

    Revert wrong commit 2979
    svn path=/trunk/; revision=2980

--- gtk/pygtk-private.h
+++ gtk/pygtk-private.h
@@ -61,6 +61,15 @@ int       _pygtk_tree_model_set_row(GtkTreeModel *model, GtkTreeIter *iter,
 				    PyObject *items);
 int       _pygtk_tree_model_remove_row(GtkTreeModel *model, GtkTreeIter *iter);
 
+/* A boxed type for GdkRegion until one gets into gtk+ itself. */
+#ifdef GDK_TYPE_REGION
+#define PYGDK_TYPE_REGION  GDK_TYPE_REGION 
+#else
+GType pygdk_region_get_type (void) G_GNUC_CONST;
+
+#define PYGDK_TYPE_REGION (pygdk_region_get_type ())
+#endif /* GDK_TYPE_REGION */
+
 void pygtk_boxed_unref_shared(PyObject *boxed);
 
 #endif
