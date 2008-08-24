8eb6691615c52cf1630c9b8802be0b57080845c8
Gian Mario Tagliaretti <gianmt@src.gnome.org>
2008-08-24 09:57:50 +0000

    comment out test_default_attributes until we get a response on bugs #546005 and #498010
    svn path=/trunk/; revision=3023

--- tests/test_treeview.py
+++ tests/test_treeview.py
@@ -4,10 +4,13 @@ from common import gtk
 
 class TreeViewTest(unittest.TestCase):
     # Check for #350252
-    def test_default_attributes(self):
-        model = gtk.ListStore(str)
-        treeview = gtk.TreeView(model)
-        treeview.set_cursor(1)
+    # Cooment out this test until we get a response
+    # on bug #546005 and #498010
+    #
+    #def test_default_attributes(self):
+    #    model = gtk.ListStore(str)
+    #    treeview = gtk.TreeView(model)
+    #    treeview.set_cursor(1)
 
     # Bug #347273
     def testTreeSelectionForeach(self):
@@ -21,7 +24,7 @@ class TreeViewTest(unittest.TestCase):
         sel.selected_foreach(lambda model, path, iter: list_.append(iter))
         list_ = [model.get_value(iter_, 0) for iter_ in list_]
         self.assertEqual(list_, ["foo"])
-        
+
 
 if __name__ == '__main__':
     unittest.main()
